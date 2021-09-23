68ebf81c6743d30a46e14d4285b335a8680eec38
yaoyongchao <yaoyongchao3g@163.com>
2021-09-23 11:57:40 +0800

    修改配置

--- app/src/main/java/com/yyc/mypack/MainActivity.kt
+++ app/src/main/java/com/yyc/mypack/MainActivity.kt
@@ -2,12 +2,11 @@ package com.yyc.mypack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yyc.mylib.YycUtil
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        YycUtil.show("1111111")
+//        YycUtil.show("1111111")
     }
 }
